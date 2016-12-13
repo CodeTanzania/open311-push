ae782f31203b1a811d76fd36d41a6110741f1837
lykmapipo <lallyelias87@gmail.com>
2016-12-14 02:29:56 +0300

    improve code comments

--- index.js
+++ index.js
@@ -10,11 +10,21 @@
  * @public
  */
 
+
 //dependencies
 const path = require('path');
 const _ = require('lodash');
 const messages = require('open311-messages');
 
+
+/**
+ * @param  {Object} [options] valid push notification transport options
+ * @param  {String} options.apiKey valid and active Google FCM API Key
+ * @param  all kue and open311-messages supported configuration options
+ * @see {@link https://github.com/Automattic/kue#redis-connection-settings}
+ * @return {Object}         push notification message transport
+ * @since 0.1.0
+ */
 exports = module.exports = function (options) {
   //merge options
   options = _.merge({ timeout: 5000 }, options);
