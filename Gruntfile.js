90f5b1d213b220d2fed9257da9071427e53d33f7
lykmapipo <lallyelias87@gmail.com>
2016-12-14 12:16:07 +0300

    update integration spec

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,17 +18,17 @@ module.exports = function (grunt) {
         src: [
           'test/**/*.js',
           'test/unit/**/*.js',
-          '!test/intergration/**/*.js'
+          '!test/integration/**/*.js'
         ]
       },
-      intergration: {
+      integration: {
         options: {
           reporter: 'spec',
           timeout: 20000
         },
         src: [
           'test/**/*.js',
-          'test/intergration/**/*.js',
+          'test/integration/**/*.js',
           '!test/unit/**/*.js'
         ]
       }
@@ -70,6 +70,6 @@ module.exports = function (grunt) {
   //custom tasks
   grunt.registerTask('default', ['jshint', 'mochaTest:unit', 'watch']);
   grunt.registerTask('test', ['jshint', 'mochaTest:unit']);
-  grunt.registerTask('intergration', ['jshint', 'mochaTest:intergration']);
+  grunt.registerTask('integration', ['jshint', 'mochaTest:integration']);
 
 };
